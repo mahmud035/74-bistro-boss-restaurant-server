0488079a75012cdad7f5b02a48fa4848912ed9dd
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-28 01:01:45 +0600

    install morgan

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const { MongoClient } = require('mongodb');
 const app = express();
 require('colors');
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 //* Middleware
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 //* Database Connection
 const uri = process.env.URI;
